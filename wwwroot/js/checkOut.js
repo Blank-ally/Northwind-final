a0e974ea6457abcaad45552d1709661ff94504e7
Second-Son-of-Mozzarella <Mohtzee@protonmail.com>
2023-12-12 08:17:02 -0600

    [1.11]

--- wwwroot/js/checkOut.js
+++ wwwroot/js/checkOut.js
@@ -20,8 +20,27 @@ $(function(){
   var addedDiscountCodes = [];
   var addedDiscountFull = [];
   var discounts = [];
+  var cartItems = [];
+  var customerId = [];
 
-  getDiscounts()
+
+
+  getCartItems();
+
+    function getCartItems(){
+      console.log('help')
+
+      $('cartProductIDs').forEach(function(index){
+        cartItems.push( $(this).html);
+        
+      });
+
+      console.log($(this).html);
+    }
+
+  console.log(cartItems);
+
+  getDiscounts();
   
   function getDiscounts() {
 
@@ -46,20 +65,29 @@ $(function(){
 
 
 $('#DiscountSb').on('click', function(){
-
+    
 
   if(!addedDiscountCodes.includes($('#DiscountCd').val())){
 
+
     for(var i = 0; i < discounts.length; i++){
       if($('#DiscountCd').val() == discounts[i].code){
+        // if(){
+
         addedDiscountCodes.push($('#DiscountCd').val());
         addedDiscountFull.push(discounts[i]);
         alert('discount ' + discounts[i].title + ' added for ' + (discounts[i].discountPercent * 100) + '%');
         $('#discontsAdded').append('<br>' + discounts[i].product.productName + ': ' + (discounts[i].discountPercent * 100) + '%');
         $('#DiscountCd').val('');
+          
+        // }else{
+        //   alert(discounts[i].productName + ' not in cart');
+        // }
+        
         break;
       }else{
         console.log('nope');
+        $('#DiscountCd').val('');
       }
     }
 
@@ -71,22 +99,12 @@ $('#DiscountSb').on('click', function(){
 
   })
 
-  var cartItems = [];
-  var customerId = [];
+  
 
 
- 
+  
 
 
-  $(document).on('change', function(){
 
-    
 
-    $('.indProdTotal').each(function( index ){
-      console.log($( this ).html())
-    })
-
-    $('#totalCheckOut').html((parseInt($('#total_cart_value').html()) + 25.50))
-
-  });
 })
